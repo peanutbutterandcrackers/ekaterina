db2fe5f1e811e5963f9f5c967a725fe0d6ab6158
Prafulla Giri <pratheblackdiamond@gmail.com>
2020-11-03 20:26:40 +0545

    Update guix-env-manifest.scm
    This commit is only for (dependency) documentation.

--- guix-env-manifest.scm
+++ guix-env-manifest.scm
@@ -3,3 +3,12 @@
 (specifications->manifest
  (list "python"
        "python-pytest"))
+
+;; guix-supplied gnucash does not have it's python bindings enabled.
+;; I have sent in a patch: https://issues.guix.gnu.org/44309 but in
+;; the meantime, I am working with gnucash-python readily available
+;; on my local machine (using the first patch in the thread). Once
+;; the patch is merged, the list above will probably include the
+;; required package. Hopefully, "gnucash:python". But for now, this
+;; environment manifest file does not create a ready-to-develop-in
+;; environment. Gnucash python bindings are missing. For now.
