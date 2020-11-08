eec19f6dcb530983b2d38c34fb57cf18035b27d8
Prafulla Giri <pratheblackdiamond@gmail.com>
2020-11-08 16:55:37 +0545

    readers: ods_reader: Add Read() function
    For now, this is just a wrapper around the csv_reader.Read()

--- ekaterina/readers/ods_reader.py
+++ ekaterina/readers/ods_reader.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+# Wait... ODSReader is just CSVReader Wrapper?
+# ............................always has been.
+
 from subprocess import run
+from tempfile import TemporaryDirectory
 
 from ekaterina.utils.fsutils import *
+from ekaterina.readers import csv_reader
 
 class CSVConversionError(Exception):
     pass
@@ -20,16 +25,16 @@ def generate_csv_from_ods_using_libreoffice(odsfile, outdir=os.getcwd()):
     outdir = standardize_path(outdir)
     odsfile = standardize_path(odsfile)
     assert isdirectory(outdir), ("Invalid Output Directory: '{}'"
-                                   .format(outdir))
+                                 .format(outdir))
     if not isfile(odsfile):
         raise CSVConversionError(("Specified .ods file '{}' not found"
                                   .format(odsfile)))
-    
+
     try:
         run(["libreoffice", "--version"], capture_output=True)
     except FileNotFoundError:
         raise CSVConversionError("LibreOffice not found.")
-    
+
     conversion = run(["libreoffice",
                       "--convert-to", "csv",
                       "--outdir", outdir,
@@ -45,3 +50,10 @@ def generate_csv_from_ods_using_libreoffice(odsfile, outdir=os.getcwd()):
         raise CSVConversionError(err_msg)
 
     return destination_path(outdir, odsfile, new_extension=".csv")
+
+def Read(odsfile):
+    tempdir = TemporaryDirectory()
+    csvfile = generate_csv_from_ods_using_libreoffice(
+        standardize_path(odsfile),
+        outdir=tempdir.name)
+    return csv_reader.Read(csvfile)
